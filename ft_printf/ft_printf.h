ee976c9576c12e6a5e2ed3cfd53091015e8538a4
wenjuin <lowwenjuin27@gmail.com>
2023-11-05 13:00:48 +0800

    done

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -17,7 +17,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int	ft_putchar(char c);
+char	ft_putchar(char c);
 int	ft_putstr(char *str);
 int	ft_putnbr(int n);
 int	ft_putnbr_unsigned(unsigned int nb);
--- ft_printf/ft_putchar.c
+++ ft_printf/ft_putchar.c
@@ -12,7 +12,7 @@
 
 #include "ft_printf.h"
 
-int	ft_putchar(char c)
+char	ft_putchar(char c)
 {
 	write(1, &c, 1);
 	return (1);
